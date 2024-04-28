40964e76c53865a25c000b2461c38ae239ef92d6
canepari-luke <can20006@byui.edu>
2024-04-27 23:38:54 -0600

    Completed Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int GradeScore = int.Parse(Console.ReadLine());
+        if (GradeScore >= 90)
+        {
+            Console.WriteLine("You got an A!");
+        }
+        else if (GradeScore >= 80)
+        {
+            Console.WriteLine("You got a B!");
+        }
+        else if (GradeScore >= 70)
+        {
+            Console.WriteLine("You got a C!");
+        }
+        else if (GradeScore >= 60)
+        {
+            Console.WriteLine("You got a D!");
+        }
+        else if (GradeScore < 60)
+        {
+            Console.WriteLine("You got an F!");
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid number.");
+        }
+        
+        if (GradeScore >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed. Don't give up!");
+        }
     }
 }
