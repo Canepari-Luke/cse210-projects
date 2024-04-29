99d4f9e883b5b9df8255a1b44cdf87db35e029af
canepari-luke <can20006@byui.edu>
2024-04-29 09:58:10 -0600

    I don't konw why mine isn't working. I'm gonna try and figure it out.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,13 +1,18 @@
-using System;
-
-class Program
+for (int y = 9; y > 3; y--)
 {
-    static void Main(string[] args)
-    {
-        for (int x=0; x=9; x++)
-        {
-            Console.Writeline("Howdy Sandbox World!")
-        }
-    }
+    Console.WriteLine("Howdy Sandbox World!");
 }
-//This is in class practice.
+
+// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         for (int x=0; x>9; x++)
+//         {
+//             Console.WriteLine("Howdy Sandbox World!");
+//         }
+//     }
+// }
+// //This is in class practice.
