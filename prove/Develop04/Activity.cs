e5197e6321ed2b0faddbfa98830fbf8225a8cf33
canepari-luke <can20006@byui.edu>
2024-06-10 10:00:10 -0600

    starting point for the mindfulness activity.

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,15 @@
+class Activity
+{
+    // attributes
+    protected string _title;
+    protected string _description;
+    protected int _duration;
+
+    // constructor
+    public Activity(string title, string description, int duration)
+    {
+        _title = title;
+        _description = description;
+        _duration = duration;
+    }
+}
---
+++ prove/Develop04/Breathing.cs
@@ -0,0 +1,8 @@
+class BreathingActivity : Activity // Inherit from the Activity class
+{
+    // Constructor
+    public BreathingActivity(string title, string description, int duration) : base(title, description, duration)
+    {
+
+    }
+}
